8bfb3fe68ced054b2793e1872d268eed7046c8d6
Maël DONNART <mael.donnart1@gmail.com>
2025-11-25 22:27:17 +0100

    chore: update app config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   srcDir: "app",
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
   modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
-  pwa: {
-    registerType: "autoUpdate",
-    manifest: {
-      name: "Finger Chooser Game",
-      short_name: "Finger Chooser",
-      description: "Finger Chooser Game",
-      display: "standalone",
-      theme_color: "#000000",
-      background_color: "#000000",
-      icons: [
-        {
-          src: "/pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-        {
-          src: "/pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any maskable",
-        },
-      ],
-    },
-    workbox: {
-      cleanupOutdatedCaches: true,
-      globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallbackAllowlist: [/^\/$/],
-    },
-  },
   app: {
+    baseURL: "/finger-chooser/",
     head: {
       title: "Finger Chooser",
       meta: [
@@ -57,9 +25,49 @@ export default defineNuxtConfig({
         },
       ],
       link: [
-        { rel: "manifest", href: "/manifest.webmanifest" },
-        { rel: "apple-touch-icon", href: "/pwa-192x192.png" },
+        { rel: "manifest", href: "/finger-chooser/manifest.webmanifest" },
+        { rel: "apple-touch-icon", href: "/finger-chooser/pwa-192x192.png" },
       ],
+    },
+  },
+  nitro: {
+    preset: "github-pages",
+  },
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Finger Chooser Game",
+      short_name: "Finger Chooser",
+      description: "Finger Chooser Game",
+      start_url: "/finger-chooser/",
+      scope: "/finger-chooser/",
+      display: "standalone",
+      theme_color: "#000000",
+      background_color: "#000000",
+      icons: [
+        {
+          src: "/finger-chooser/pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+        {
+          src: "/finger-chooser/pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    workbox: {
+      cleanupOutdatedCaches: true,
+      globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+      navigateFallback: "/finger-chooser/",
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
     },
   },
 });
