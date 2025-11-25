86286ec95ea2f5de782f1a56954d45c76193854c
Maël DONNART <mael.donnart1@gmail.com>
2025-11-25 22:59:31 +0100

    remove router

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,25 @@
+<script setup lang="ts">
+import { ref, onMounted, computed } from 'vue';
+import LandingPage from '~/components/LandingPage.vue';
+import FingerGame from '~/components/FingerGame.vue';
+
+const isTouchDevice = ref(false);
+const forced = ref(false);
+
+onMounted(() => {
+  const hasTouch = navigator.maxTouchPoints > 0 || 'ontouchstart' in window;
+  isTouchDevice.value = hasTouch;
+});
+
+const shouldShowGame = computed(() => isTouchDevice.value || forced.value);
+const handleForceStart = () => {
+  forced.value = true;
+};
+</script>
+
 <template>
   <div>
-    <NuxtPage />
+    <FingerGame v-if="shouldShowGame" />
+    <LandingPage v-else @force-start="handleForceStart" />
   </div>
 </template>
