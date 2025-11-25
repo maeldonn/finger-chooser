72ee06b395663663fed54879b307ccbe0f58a34e
Maël DONNART <mael.donnart1@gmail.com>
2025-11-26 00:22:16 +0100

    fix: vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/finger-chooser/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
