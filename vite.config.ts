39246815fcf585f898064bb1e4809145b9047cb6
Maël DONNART <mael.donnart1@gmail.com>
2025-11-26 00:07:33 +0100

    fix: improvements

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '/' : '/finger-chooser/',
