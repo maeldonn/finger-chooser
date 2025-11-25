f4091b505887b49cfc17c0c2ceade21f03aefbcc
Maël DONNART <mael.donnart1@gmail.com>
2025-11-25 18:27:42 +0100

    fix: update colors

--- app/utils/colors.ts
+++ app/utils/colors.ts
@@ -1,9 +1,23 @@
 export const NEON_COLORS = [
   "#FF0055",
+  "#00E0FF",
+  "#F8FF00",
+  "#9B00FF",
+  "#00FF75",
+  "#FF6B00",
   "#00FFDD",
-  "#0080FF",
-  "#FFFF00",
   "#FF00FF",
+  "#00B2FF",
+  "#FFD300",
+  "#4CFF00",
+  "#FF0090",
+  "#00FFC6",
+  "#FF3D00",
+  "#0090FF",
+  "#FFE200",
+  "#7700FF",
+  "#56FFB2",
+  "#FF9900",
 ];
 
 export const getFingerColor = (index: number): string => {
