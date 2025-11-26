450f39f9bf3a3dd0d4ada5c959d732aaf9b33686
Maël DONNART <mael.donnart1@gmail.com>
2025-11-26 13:40:53 +0100

    feat: PWA app

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
@@ -6,13 +7,34 @@ import path from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Finger Chooser Game',
+        short_name: 'Finger Chooser',
+        description: 'Finger Chooser Game',
+        start_url: '/finger-chooser/',
+        scope: '/finger-chooser/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#000000',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '~/': path.resolve(__dirname, 'src') + '/',
       '@/': path.resolve(__dirname, 'src') + '/',
     },
   },
-}));
+});
